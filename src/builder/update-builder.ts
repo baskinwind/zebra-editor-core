bd60543dea3f39c148be8e1c4625e097bb3c88b5
aco <wwsxuan@163.com>
2020-05-24 23:09:49 +0800

    移除无用代码

--- src/builder/update-builder.ts
+++
@@ -1,8 +0,0 @@
-import Article from "../components/article";
-
-export default {
-  updateArticle(component: Article) {
-    let article = document.getElementById(component.id);
-    article?.parentElement?.replaceChild(component.render(), article);
-  },
-};
--- src/event-handle/index.ts
+++
@@ -1,9 +0,0 @@
-import { $on } from "../event";
-
-const registerEvent = (handle: any) => {
-  Object.keys(handle).forEach((key) => {
-    $on(key, handle[key]);
-  });
-};
-
-export default registerEvent;
