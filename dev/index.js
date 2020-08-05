518c41fcce6708662c9c14f1bbb5ed3ee8a32865
aco <wwsxuan@163.com>
2020-08-05 16:56:28 +0800

    demo 的样式问题

--- dev/index.js
+++ dev/index.js
@@ -3,8 +3,12 @@ import article from "./article";
 import { mount, createByRaw } from "../src";
 
 mount("root", article, {
-  onError(err) {
-    console.dir(err);
+  beforeCreate(doc) {
+    let defaultArticleTheme = doc.createElement("link");
+    defaultArticleTheme.href = "https://zebrastudio.tech/article-theme/default.css";
+    defaultArticleTheme.rel = "stylesheet";
+    defaultArticleTheme.type = "text/css";
+    doc.head.appendChild(defaultArticleTheme);
   }
 });
 
